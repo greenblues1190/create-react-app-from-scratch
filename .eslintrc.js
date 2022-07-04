dcb5f2f80708a166175599017a65b5ab5950377e
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-04 16:48:26 +0900

    chore: 툴체인 설정
    - react
    - babel
    - webpack
    - typescript
    - eslint
    - prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    '@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', '@emotion'],
+  rules: {
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default App;
